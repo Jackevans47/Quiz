b8854224b24df18b0e1c4e9e6035cc31a171937c
jackevans47 <jack_evans47@outlook.com>
2024-01-10 17:31:35 +0000

    Add welcome message to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 const beginQuiz = document.getElementById('start')
 const questionElement = document.getElementById('question-holder')
+const welcomeMessage = document.getElementById('welcome')
 
 beginQuiz.addEventListener('click', startQuiz)
 
 function startQuiz() {
     console.log('start')
     beginQuiz.classList.add('hide')
+    welcomeMessage.classList.add('hide')
     questionElement.classList.remove('hide')
 }
 
