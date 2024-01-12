ae90c5279ddd00f10027077c9c51fb2e38b192d0
jackevans47 <jack_evans47@outlook.com>
2024-01-12 20:01:05 +0000

    Add function to display question plus add basic questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const beginQuiz = document.getElementById('start')
-const questionElement = document.getElementById('question-holder')
+const questionholderElement = document.getElementById('question-holder')
 const welcomeMessage = document.getElementById('welcome')
+const questionElement = document.getElementById('questions')
+const answerButtonsElement = document.getElementById('answers')
+
+let randomQuestions, questionIndex
 
 beginQuiz.addEventListener('click', startQuiz)
 
@@ -8,12 +12,42 @@ function startQuiz() {
     console.log('start')
     beginQuiz.classList.add('hide')
     welcomeMessage.classList.add('hide')
-    questionElement.classList.remove('hide')
+    questionholderElement.classList.remove('hide')
+    randomQuestions = questions.sort(() => Math.random())
+    questionIndex = 0
+    nextQuestion();
 }
 
 function nextQuestion() {
-
+    displayQuestion(randomQuestions[questionIndex])
 }
+
+function displayQuestion(question) {
+    questionElement.innerHTML = question.question
+}
+
 function chooseAnswer() {
 
 }
+
+const questions = [
+    {
+        question: "Which of these animals are considered mans best friend",
+        answers: [
+            { text: "Cat", correct: false },
+            { text: "Dog", correct: true },
+            { text: "Hamster", correct: false },
+            { text: "Bird", correct: false },
+        ]
+    },
+    {
+        question: "Which of these animals are considered mans best friend",
+        answers: [
+            { text: "Cat", correct: false },
+            { text: "Dog", correct: true },
+            { text: "Hamster", correct: false },
+            { text: "Bird", correct: false },
+        ]
+    }
+
+]
