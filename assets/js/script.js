bc7ef723b4e6ffb85f7fd37f43415434f48c6840
jackevans47 <jack_evans47@outlook.com>
2024-01-13 17:13:46 +0000

    Remove placement answer and add answers for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const beginQuiz = document.getElementById('start')
+const nextButton = document.getElementById('next')
 const questionholderElement = document.getElementById('question-holder')
 const welcomeMessage = document.getElementById('welcome')
 const questionElement = document.getElementById('questions')
@@ -13,41 +14,66 @@ function startQuiz() {
     beginQuiz.classList.add('hide')
     welcomeMessage.classList.add('hide')
     questionholderElement.classList.remove('hide')
-    randomQuestions = questions.sort(() => Math.random())
+    randomQuestions = questions.sort(() => Math.random() - .5)
     questionIndex = 0
     nextQuestion();
 }
 
 function nextQuestion() {
     displayQuestion(randomQuestions[questionIndex])
+    resetAnswer()
 }
 
 function displayQuestion(question) {
-    questionElement.innerHTML = question.question
+    resetState();
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', chooseAnswer)
+        answerButtonsElement.appendChild(button);
+    })
 }
+
 
 function chooseAnswer() {
 
 }
 
+function resetState() {
+    nextButton.style.display = 'none';
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
 const questions = [
     {
-        question: "Which of these animals are considered mans best friend",
+        question: "Which of these animals are considered to be mans best friend?",
         answers: [
-            { text: "Cat", correct: false },
-            { text: "Dog", correct: true },
-            { text: "Hamster", correct: false },
-            { text: "Bird", correct: false },
+            { text: 'Cat', correct: false },
+            { text: 'Dog', correct: true },
+            { text: 'Hamster', correct: false },
+            { text: 'Bird', correct: false },
         ]
     },
     {
-        question: "Which of these animals are considered mans best friend",
+        question: "What is 5 x 2?",
         answers: [
-            { text: "Cat", correct: false },
-            { text: "Dog", correct: true },
-            { text: "Hamster", correct: false },
-            { text: "Bird", correct: false },
+            { text: "15", correct: false },
+            { text: "10", correct: true },
+            { text: "25", correct: false },
+            { text: "30", correct: false },
+        ]
+    },
+    {
+        question: 'What is 4 * 2?',
+        answers: [
+            { text: '6', correct: false },
+            { text: '8', correct: true }
         ]
     }
-
 ]
