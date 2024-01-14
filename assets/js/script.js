65462e8dec3a39d537ea6f861ca6f713a44a0bdf
jackevans47 <jack_evans47@outlook.com>
2024-01-14 10:42:03 +0000

    Change classlist for button to impliment styling to answer button

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ function displayQuestion(question) {
     question.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerText = answer.text;
-        button.classList.add('btn');
+        button.classList.add('button');
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
