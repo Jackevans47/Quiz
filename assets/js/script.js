de196821bf48e7afe6576430630b27e44feca085
jackevans47 <jack_evans47@outlook.com>
2024-01-22 16:54:29 +0000

    Add remaining questions to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,148 +1,139 @@
+/* jshint esversion: 6 */
 import questionData from './questions.json' assert { type: 'json' };
 
-/* jshint esversion: 6 */
-const beginQuiz = document.getElementById('start')
-const nextButton = document.getElementById('next')
-const questionholderElement = document.getElementById('question-holder')
-const welcomeMessage = document.getElementById('welcome')
-const questionElement = document.getElementById('questions')
-const answerButtonsElement = document.getElementById('answers')
-const pointsElement = document.getElementsByClassName('points-num')
-const questionTally = document.getElementById('current-question-number')
-const home = document.getElementById('home')
-const dialog = document.getElementById('dialog')
-const confirmElement = document.getElementById('confirm')
-const completionElement = document.getElementById('completion')
-const playAgain = document.getElementById('restart')
 
-let randomQuestions, questionIndex, points, questionNum
+const beginQuiz = document.getElementById('start');
+const nextButton = document.getElementById('next');
+const questionholderElement = document.getElementById('question-holder');
+const welcomeMessage = document.getElementById('welcome');
+const questionElement = document.getElementById('questions');
+const answerButtonsElement = document.getElementById('answers');
+const pointsElement = document.getElementsByClassName('points-num');
+const questionTally = document.getElementById('current-question-number');
+const home = document.getElementById('home');
+const dialog = document.getElementById('dialog');
+const confirmElement = document.getElementById('confirm');
+const completionElement = document.getElementById('completion');
+const playAgain = document.getElementById('restart');
+
+let randomQuestions, questionIndex, points, questionNum;
 
 
 home.addEventListener("click", () => {
     dialog.showModal();
 });
-beginQuiz.addEventListener('click', startQuiz)
-confirmElement.addEventListener('click', returnHome)
+beginQuiz.addEventListener('click', startQuiz);
+confirmElement.addEventListener('click', returnHome);
 nextButton.addEventListener('click', () => {
-    console.log(questionIndex, randomQuestions.length)
     if (questionIndex == randomQuestions.length - 1) {
-        questionholderElement.classList.add('hide')
-        completionElement.classList.remove('hide')
-        completionElement.classList.add('flex-center')
-        resetState()
+        questionholderElement.classList.add('hide');
+        completionElement.classList.remove('hide');
+        completionElement.classList.add('flex-center');
+        resetState();
     } else {
-        questionIndex++
-        nextQuestion()
+        questionIndex++;
+        nextQuestion();
     }
 
-})
-playAgain.addEventListener('click', returnHome)
+});
+playAgain.addEventListener('click', returnHome);
 
 /** Return home function  */
 function returnHome() {
     resetState();
-    console.log("return home")
-    completionElement.classList.add('hide')
-    questionholderElement.classList.add('hide')
-    welcomeMessage.classList.remove('hide')
-    beginQuiz.innerText = 'Start'
-    beginQuiz.classList.remove('hide')
+    completionElement.classList.add('hide');
+    questionholderElement.classList.add('hide');
+    welcomeMessage.classList.remove('hide');
+    beginQuiz.innerText = 'Start';
+    beginQuiz.classList.remove('hide');
 }
 
 /**  Start Quiz Function  */
 function startQuiz() {
-    console.log(questions)
-    console.log('start')
-    beginQuiz.classList.add('hide')
-    welcomeMessage.classList.add('hide')
-    questionholderElement.classList.remove('hide')
-    randomQuestions = questionData["questions"].sort(() => Math.random() - .5)
-    questionIndex = 0
-    questionNum = 0
-    setPoints(0)
+    beginQuiz.classList.add('hide');
+    welcomeMessage.classList.add('hide');
+    questionholderElement.classList.remove('hide');
+    randomQuestions = questionData["questions"].sort(() => Math.random() - .5);
+    questionIndex = 0;
+    questionNum = 0;
+    setPoints(0);
     nextQuestion();
 }
 
 /**   Next Question function */
 function nextQuestion() {
-    console.log('next-question')
-    resetState()
-    displayQuestion(randomQuestions[questionIndex])
+    resetState();
+    displayQuestion(randomQuestions[questionIndex]);
 }
 
 /** Display question function */
 function displayQuestion(question) {
-    console.log('display-question')
     resetState();
-    questionNum++
-
-    questionTally.innerText = questionNum
-    questionElement.innerText = questionNum + ". " + question.question
+    questionNum++;
+    questionTally.innerText = questionNum;
+    questionElement.innerText = questionNum + ". " + question.question;
     question.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerText = answer.text;
         button.classList.add('answer-button');
         if (answer.correct) {
-            button.dataset.correct = answer.correct
+            button.dataset.correct = answer.correct;
         }
-        button.addEventListener('click', chooseAnswer)
+        button.addEventListener('click', chooseAnswer);
         answerButtonsElement.appendChild(button);
-    })
+    });
 }
 
 // Reset back to default state everytime a new question is set
 function resetState() {
-    clearStatusClass(document.body)
-    console.log('reset')
-    nextButton.classList.add('hide')
+    clearStatusClass(document.body);
+    nextButton.classList.add('hide');
     while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
 }
 
 /** Points */
 function setPoints(newPoints) {
-    points = newPoints
+    points = newPoints;
     Array.from(pointsElement).forEach(pointElement => {
-        pointElement.innerText = points
+        pointElement.innerText = points;
     });
 }
 
 /**  Choose answer function */
 function chooseAnswer(e) {
-    console.log('choose-answer')
-    const selectedButton = e.target
-    const correct = !!selectedButton.dataset.correct
-    console.log(correct)
+    const selectedButton = e.target;
+    const correct = !!selectedButton.dataset.correct;
     if (correct) {
-        setPoints(points + 1)
+        setPoints(points + 1);
     }
 
-    setStatusClass(document.body, correct)
+    setStatusClass(document.body, correct);
 
     Array.from(answerButtonsElement.children).forEach(button => {
         if (button.dataset.correct === "true") {
-            button.classList.add("correct")
+            button.classList.add("correct");
         }
         button.disabled = true;
     });
 
     Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    nextButton.classList.remove('hide')
+        setStatusClass(button, button.dataset.correct);
+    });
+    nextButton.classList.remove('hide');
 }
 
 function setStatusClass(value, correct) {
-    clearStatusClass(value)
+    clearStatusClass(value);
     if (correct) {
-        value.classList.add('button-correct')
+        value.classList.add('button-correct');
     } else {
-        value.classList.add('button-incorrect')
+        value.classList.add('button-incorrect');
     }
 }
 
 function clearStatusClass(value) {
-    value.classList.remove('button-correct')
-    value.classList.remove('button-incorrect')
+    value.classList.remove('button-correct');
+    value.classList.remove('button-incorrect');
 }
